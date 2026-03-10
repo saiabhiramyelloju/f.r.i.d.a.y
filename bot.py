460c1fca5939838f6e8ef23d66adbfa5007da4f1
saiabhiramyelloju <abhi.yelloju@gmail.com>
2026-03-10 21:31:32 +0530

    added project files

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder,MessageHandler, filters, ContextTypes
+
+TOKEN = "8682800667:AAEpig_1D8alwQT_9o9lhp0xJ9qCS0kRuYM"
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message:
+        user_message = update.message.text
+        await update.message.reply_text(f"You said: {user_message}")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT,reply))
+
+app.run_polling()
