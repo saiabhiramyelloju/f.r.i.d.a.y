9c291939e3d5a816c968e9f23da5b4566740ec11
saiabhiramyelloju <abhi.yelloju@gmail.com>
2026-03-13 12:52:05 +0530

    added calculations for two numbers

--- intent.py
+++ intent.py
@@ -17,14 +17,70 @@ def detect_intent(message):
 def process_message(message):
     print("Processing: ",message)
 
-    greetings = ["hello","hi"]
+    #keywords that messages have
+    greetings = ["/start","hello","hi"]
     wishes = [
         "good morning",
         "good afternoon",
         "good evening",
         "good night"
     ]
+    calculations = [
+        "add",
+        "subtract",
+        "multiply",
+        "divide",
+        "to the power"
+    ]
+    #greetings
     for word in greetings:
         if word == message.lower():
-            return random.choice(greetings) + ", I am F.R.I.D.A.Y"    
-    return "i dont understand"
+            return "Hello, I am F.R.I.D.A.Y\nHow can i help you today?"
+    #wishes
+    for word in wishes:
+        if word == message.lower():
+            return message+" ,sir"
+    #creater of friday response
+    if message.lower() == ("who created you" or "founder of friday" or "creater of you"):
+        return "AbhiRam"
+    #addition
+    if message.lower() == "/add":
+        return "give me two numbers to for addition with spaces between\nExample: 2 3"
+    numbers = message.split()
+    if len(numbers) == 2:
+        numbers[0].isdigit()
+        numbers[1].isdigit()        
+        a = int(numbers[0])
+        b = int(numbers[1])
+        result = a + b
+    #subtraction
+    if message.lower() == "/subtract":
+        return "give me two numbers to for subtraction with spaces between\nExample: 2 3"
+    numbers = message.split()
+    if len(numbers) == 2:
+        numbers[0].isdigit()
+        numbers[1].isdigit()        
+        a = int(numbers[0])
+        b = int(numbers[1])
+        result = a - b
+    #multiplication
+    if message.lower() == "/muliply":
+        return "give me two numbers to for multiplication with spaces between\nExample: 2 3"
+    numbers = message.split()
+    if len(numbers) == 2:
+        numbers[0].isdigit()
+        numbers[1].isdigit()        
+        a = int(numbers[0])
+        b = int(numbers[1])
+        result = a * b
+    #division
+    if message.lower() == "/divide":
+        return "give me two numbers to for division with spaces between\nExample: 2 3"
+    numbers = message.split()
+    if len(numbers) == 2:
+        numbers[0].isdigit()
+        numbers[1].isdigit()        
+        a = int(numbers[0])
+        b = int(numbers[1])
+        result = a / b
+    return "I coudn't process your request as am under development\nPlease try again later"
